c9377305f52b9a7df9fe7f48dd0ad43b03ce7826
Andrii Starusiev <a.starusiev@zajno.com>
2019-10-13 17:49:28 +0300

    resources responsive

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,21 +1,20 @@
 const page = document.querySelector('body');
+const errorClassName = 'error';
+
+/**
+ * @param {string} email 
+ * @returns {boolean}
+ */
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
 
 if (page.id === 'contacts-page') {
     const form = document.getElementById('contact-form');
     const emailWrap = form.querySelector('.email-wrap');
     const emailInput = emailWrap.querySelector('input');
     const textarea = form.querySelector('textarea');
-    
-    /**
-     * @param {string} email 
-     * @returns {boolean}
-     */
-    function validateEmail(email) {
-        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    }
-    
-    const errorClassName = 'error';
     
     function onSubmit(e) {
         e.preventDefault();
@@ -33,4 +32,28 @@ if (page.id === 'contacts-page') {
     }
     
     form.addEventListener('submit', onSubmit);
+}
+
+
+const subscribeForm = document.getElementById('subscribe-form');
+
+if (subscribeForm) {
+    const emailWrap = subscribeForm.querySelector('.email-wrap');
+    const emailInput = emailWrap.querySelector('input');
+
+    function onSubmit(e) {
+        e.preventDefault();
+    
+        const email = emailInput.value;
+    
+        if (!validateEmail(email)) {
+            emailWrap.classList.add(errorClassName);
+            return;
+        }
+    
+        emailWrap.classList.remove(errorClassName);
+        alert(`submitting the form with email: ${email}`);
+    }
+    
+    subscribeForm.addEventListener('submit', onSubmit);
 }
