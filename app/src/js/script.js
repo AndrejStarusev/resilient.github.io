36cd7a1c756271abb0f44cc700d521491cd61756
Andrii Starusiev <a.starusiev@zajno.com>
2019-10-14 14:20:18 +0300

    mobile menu, tinified images, w3c validation

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -57,3 +57,17 @@ if (subscribeForm) {
     
     subscribeForm.addEventListener('submit', onSubmit);
 }
+
+
+// ==== menu
+const burger = document.getElementById('burger');
+const menu = document.getElementById('mobile-menu');
+
+function toggleMenu() {
+    const active = 'active';
+
+    this.classList.toggle(active);
+    menu.classList.toggle(active);
+}
+
+burger.addEventListener('click', toggleMenu);
